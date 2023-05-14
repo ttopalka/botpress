534d2cda71168a21b80b71183756afbe8262eaf6
ttopalka <133214931+ttopalka@users.noreply.github.com>
2023-05-15 01:59:30 +0200

    Add files via upload

---
+++ py2pdfAPI.py
@@ -0,0 +1,28 @@
+import PyPDF2
+
+# Chemin vers le fichier PDF que vous souhaitez lire
+pdf_path = 'cukhaq13cbfw5ph94wi4-ROI%20(1).pdf'
+
+# Fonction pour extraire le texte de chaque page du PDF
+def extract_text_from_pdf():
+    try:
+        # Ouverture du fichier PDF
+        with open(pdf_path, 'rb') as file:
+            # Chargement du lecteur PDF
+            reader = PyPDF2.PdfReader(file)
+
+            # Parcours de chaque page du PDF
+            for page_number in range(reader.numPages):
+                # Obtention de l'objet de la page
+                page = reader.getPage(page_number)
+
+                # Récupération du contenu textuel de la page
+                page_text = page.extract_text()
+
+                # Affichage du texte de la page
+                return page_text(f"Page {page_number + 1}: {page_text}")
+    except Exception as e:
+        return page_text(f"Une erreur s'est produite lors de la lecture du fichier PDF : {e}")
+
+# Appel de la fonction pour extraire le texte du PDF
+extract_text_from_pdf()
